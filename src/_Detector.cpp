0e6134c7f99f44d0e1f6cb5fc8568d81a9ef6218
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-06-30 10:17:26 +0900

    added Detector

--- src/_Detector.cpp
+++ src/_Detector.cpp
@@ -40,3 +40,34 @@ std::shared_ptr<Mat> _DETECTOR::getImageRoiInGreyScale(std::shared_ptr<Mat> img)
 	return std::make_shared<Mat>(gray);
 	//return std::make_shared<Mat>(temp(roi));
 }
+
+std::vector<std::vector<Point>> _DETECTOR::detectBolts(std::shared_ptr<Mat> roi, int threshold) {
+//	Mat drawing = roi->clone();
+    Mat image = *roi.get();
+	Mat binary;
+
+    Scalar mean = cv::mean(image);
+	float sigma = 0.33;
+	int lowerGradThresh = std::max(0, int((1.0 - sigma) * mean[0]));
+	int upperGradThresh = std::min(255, int((1.0 + sigma) * mean[0]));
+
+	//cv::threshold(*roi.get(), binary, mean[0] * 1.75, 255, THRESH_BINARY);
+	
+    Canny(image, binary, lowerGradThresh, upperGradThresh);
+//    cv::morphologyEx(binary, binary, MORPH_CLOSE, cv::noArray(), cv::Point(-1, -1), 2);
+    cv::dilate(binary, binary, cv::noArray());
+    imshow("binary", binary);
+    waitKey(1);
+
+	std::vector<std::vector<Point> > contours;
+	std::vector<Vec4i> hierarchy;
+
+	/// Detect edges using canny
+	/// Find contours
+	findContours(binary, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+//	std::cout << contours.size() << std::endl;
+//	drawContours(drawing, contours, -1, (100, 255, 255));
+//    imshow("contours", drawing);
+//    waitKey(0);
+	return contours;
+}
