93c5b2b154196923e7ff2ce58afe7d3ea42488c7
Wataru Oshima <wataru.bb.tokyo@gmail.com>
2022-06-30 13:17:45 +0900

    editted some details

--- src/bolt_detector.cpp
+++ src/bolt_detector.cpp
@@ -266,7 +266,7 @@ int main( int argc, char** argv )
           Scalar(118, 185, 0), //font color
           2);
         putText(cc.src, mode, Point(10, 80), FONT_HERSHEY_DUPLEX,1.0,Scalar(0, 0, 255), 2);
-        putText(cc.src, cmd_exp, Point(10, IMG_HIEHGT-50), FONT_HERSHEY_DUPLEX, 1.0, Scalar(255,0, 0), 2);
+        putText(cc.src, cmd_exp, Point(10, (IMG_HEIGHT-50)), FONT_HERSHEY_DUPLEX, 1.0, Scalar(255,0, 0), 2);
         imshow( "src", cc.src);
         waitKey(3);
       }
